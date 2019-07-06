b51bdd73fd445a1bc9900ef6229a61af2f47b56b
asrulsibaoel <asrul@kata.ai>
2019-07-06 09:52:25 +0700

    Added: predict and train class

---
+++ components/domains/predict.ts
@@ -0,0 +1,7 @@
+import { Component } from "merapi";
+
+export default class Predict extends Component {
+    constructor() {
+        super();
+    }
+}
---
+++ components/domains/train.ts
@@ -0,0 +1,7 @@
+import { Component } from "merapi";
+
+export default class Train extends Component {
+    constructor() {
+        super();
+    }
+}
