c80b61c99e9c2f207be8ec5f4d8f2e5c736ff19c
asrulsibaoel <asrul@kata.ai>
2019-07-06 09:59:17 +0700

    Added: train and check

--- components/domains/train.ts
+++ components/domains/train.ts
@@ -4,4 +4,12 @@ export default class Train extends Component {
     constructor() {
         super();
     }
+
+    public async trainRoot(word: string): Promise<boolean> {
+        return true;
+    }
+
+    public async checkAvailability(): Promise<boolean> {
+        return false;
+    }
 }
